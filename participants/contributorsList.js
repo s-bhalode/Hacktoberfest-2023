6737c2ef3afabe803e8c63672dab2d5bbd856734
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2023-10-01 09:42:42 +0530

    vaibhav

--- participants/contributorsList.js
+++ participants/contributorsList.js
@@ -4,4 +4,9 @@ contributors = [
       fullname: "Simran Bhalode",
       username: "https://github.com/s-bhalode",
     },
+    {
+      id: 2,
+      fullname: "Vaibhav Agarwal",
+      username: "https://github.com/vaibhava17",
+  }
 ]

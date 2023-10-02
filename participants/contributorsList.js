5db7aa9d01478e08ea85b920fbf517f653571982
Deepak Bhalode <51329728+deepakBhalode@users.noreply.github.com>
2023-10-02 13:50:27 +0530

    Update contributorsList.js

--- participants/contributorsList.js
+++ participants/contributorsList.js
@@ -4,4 +4,9 @@ contributors = [
       fullname: "Simran Bhalode",
       username: "https://github.com/s-bhalode",
     },
+    {
+        id: 2,
+        fullname: "Deepak Bhalode",
+        username: "https://github.com/deepakBhalode"
+    },
 ]
